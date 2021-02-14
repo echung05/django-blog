042ff937f9011b27cbbec6404aed2f41bd982c7a
Esther Chung <e.chung059@gmail.com>
2021-02-14 02:20:43 -0500

    Finalize bootstrap on thoughts form and thoughts list

--- polls/forms.py
+++ polls/forms.py
@@ -6,3 +6,5 @@ class ThoughtsForm(forms.ModelForm):
     class Meta:
         model = Thought
         fields = "__all__"
+        widgets = {'question': forms.TextInput(attrs={'size': 70, 'placeholder': 'What question is your deep thought about?'}), 'title': forms.TextInput(
+            attrs={'size': 70, 'placeholder': 'Title'}), 'body': forms.TextInput(attrs={'size': 70, 'placeholder': 'Deep thought'})}
